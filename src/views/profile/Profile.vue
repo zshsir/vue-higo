9c8911a04188ecb14f272635832e14a95b789458
ZSHsir <995801887@qq.com>
2022-04-23 09:00:32 +0800

    最新

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -53,4 +53,57 @@ export default {
 
 <style  scoped>
 
+      img {
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        margin-top: 4px;
+      }
+      .user-desc {
+        display: flex;
+        flex-direction: column;
+        margin-left: 10px;
+        line-height: 20px;
+        font-size: 14px;
+        color: #fff;
+        span {
+          color: #fff;
+          font-size: 14px;
+          padding: 2px 0;
+        }
+      }
+      .account-setting {
+        position: absolute;
+        top: 10px;
+        right: 20px;
+        font-size: 13px;
+        color: #fff;
+        .van-icon-setting-o {
+          font-size: 16px;
+          vertical-align: -3px;
+          margin-right: 4px;
+        }
+      }
+    }
+  }
+  .user-list {
+    padding: 0 8px;
+    margin-top: 40px;
+    li {
+      padding-left: 5px;
+      padding-right: 5px;
+      height: 40px;
+      line-height: 40px;
+      display: flex;
+      justify-content: space-between;
+      font-size: 14px;
+      .van-icon-arrow {
+        margin-top: 13px;
+      }
+      margin: 20px 0 !important;
+      background: #ffffff;
+      border-radius: 3px;
+    }
+  }
+}
 </style>
